0d58d2a8bffd4fdd08a80c417d4f6e335bc5b84c
paclaraujo <paclaraujo@gmail.com>
2022-01-16 11:29:51 -0300

    chore: add register store import into store config

--- src/store.js
+++ src/store.js
@@ -1,19 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import registerStore from './store/register.store.js';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        
+       registerStore 
     },
     state: {
-
+        ...registerStore.state
     },
     actions: {
-
+        ...registerStore.actions
     },
     mutations: {
-        
+        ...registerStore.mutations
+    },
+    getters: {
+        ...registerStore.getters
     }
 })
