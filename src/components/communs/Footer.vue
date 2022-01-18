0d59c8271ea712ec5e56781bee786f0ca0b4b19d
Paloma Araujo <paclaraujo@gmail.com>
2022-01-17 22:38:04 -0300

    refactor: remove social medias and search input

--- src/components/communs/Footer.vue
+++ src/components/communs/Footer.vue
@@ -1,69 +1,56 @@
 <template>
-    <v-footer
-      padless
-      :fixed="false"
-      color="primary"
-      class="pa-10 mt-0 bg-sac"
-    >
-        <v-row>
-            <v-col cols="3" class="text-left">
-                <strong>#DinEduc</strong>
-                <p class="caption mt-3 mb-6">
-                    O compromisso da DinEduc é instruir 
-                    <br/>e informar sobre finanças de forma 
-                    <br/>simples e direta para os jovens. </p>
-                <strong>#NossasRedes</strong>
-                <div class="mt-2">
-                    <v-btn icon color="secundary"><v-icon class="black--text">fab fa-youtube</v-icon></v-btn>
-                    <v-btn icon color="secundary"><v-icon class="black--text">fab fa-twitter</v-icon></v-btn>
-                    <v-btn icon color="secundary"><v-icon class="black--text">fab fa-facebook</v-icon></v-btn>
-                    <v-btn icon color="secundary"><v-icon class="black--text">fab fa-instagram</v-icon></v-btn>
-                </div>
-                
-            </v-col>
-            <v-col cols="3">
-                <div>
-                <div class="mb-2"><strong>DinEduc</strong></div>
-                <div class="mb-2"><strong>Aulas</strong></div>
-                <div class="mb-2"><strong>Voluntários</strong></div>
-                <div class="mb-2"><strong>Parcerias Escolas</strong></div>
-                <div><strong>Blog</strong></div>
-                </div>
-            </v-col>
-            <v-col cols="6">
-                <strong>#NewsLetter</strong>
-                <p class="caption mt-0">Receba novidades no seu e-mail </p>
-                <div class="d-flex">
-                    <div class="mr-3 width-50">
-                        <v-text-field class="white" hide-details="hide" outlined placeholder="E-mail" dense color="secondary"/>
-                    </div>
-                    <v-btn color="secondary">Assinar</v-btn>
-                </div>
-                <p class="caption mt-3">Central de Ajuda | Contato </p>
-            </v-col>
-        </v-row>
-
-
-        
-    </v-footer>
+  <v-footer padless :fixed="false" color="primary" class="pa-10 mt-0 bg-sac">
+    <v-row>
+      <v-col cols="3" class="text-left">
+        <strong>#DinEduc</strong>
+        <p class="caption mt-3 mb-6">
+          O compromisso da DinEduc é instruir
+          <br />e informar sobre finanças de forma <br />simples e direta para
+          os jovens.
+        </p>
+      </v-col>
+      <v-col cols="3">
+        <div>
+          <div class="mb-2"><strong>DinEduc</strong></div>
+          <div class="mb-2"><strong>Aulas</strong></div>
+          <div class="mb-2"><strong>Parcerias Escolas</strong></div>
+          <div class="mb-2"><strong>Voluntários</strong></div>
+        </div>
+      </v-col>
+      <v-col cols="6">
+        <strong>#NewsLetter</strong>
+        <p class="caption mt-0">Receba novidades no seu e-mail</p>
+        <div class="d-flex">
+          <div class="mr-3 width-50">
+            <v-text-field
+              class="white"
+              hide-details="hide"
+              outlined
+              placeholder="E-mail"
+              dense
+              color="secondary"
+            />
+          </div>
+          <v-btn color="secondary">Assinar</v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
 .width-50 {
-    width:50%
+  width: 50%;
 }
 
 .bg-sac {
-  background: url('../../assets/DashboardSac.png');
+  background: url("../../assets/DashboardSac.png");
   background-repeat: no-repeat;
   background-position: bottom right;
   background-size: auto 95%;
 }
-
 </style>
--- src/components/communs/Header.vue
+++ src/components/communs/Header.vue
@@ -1,76 +1,68 @@
 <template>
   <v-main class="main">
-      <div class="header px-10 py-2">
-          <v-app-bar
-            color="primary"
-            class="black--text"
-            app
-            outlined
-            flat
-            dark>
-            <v-row>
-              <v-col cols="6" class="py-4">
-                  <span class="mr-6">Siga-nos nas redes sociais</span>
-                  <v-btn icon color="secundary"><v-icon class="black--text">fab fa-youtube</v-icon></v-btn>
-                  <v-btn icon color="secundary"><v-icon class="black--text">fab fa-twitter</v-icon></v-btn>
-                  <v-btn icon color="secundary"><v-icon class="black--text">fab fa-facebook</v-icon></v-btn>
-                  <v-btn icon color="secundary"><v-icon class="black--text">fab fa-instagram</v-icon></v-btn>
-              </v-col>
-              <v-col cols="6" class="text-end py-5">
-                    <span>Central de Ajuda</span>
-                    <span class="mx-4">|</span>
-                    <span class="mr-12">Contato</span>
-                  <v-btn
-                    depressed 
-                    color="secondary"
-                    class="px-6"
-                    @click="$router.push('/login')"
-                    ><v-icon size="20" class="mr-2">far fa-user-circle</v-icon>Login</v-btn>
-              </v-col>
-          </v-row>
-          </v-app-bar>
-          <v-row class="">
-              <v-col cols="3" class="py-0">
-                <div class="bg-logo"></div>
-              </v-col>
-              <v-col cols="6" class="pt-4" align="center"> 
-                    <v-btn text color="secondary">DinEduc</v-btn>
-                    <v-btn text color="secondary">Aulas</v-btn>
-                    <v-btn text color="secondary">Voluntários</v-btn>
-                    <v-btn text color="secondary">Parcerias Escolas</v-btn>
-                    <v-btn text color="secondary">Blog</v-btn>
-              </v-col>
-              <v-col cols="3" class="pt-0 pl-10">
-                  <v-text-field 
-                  outlined
-                  append-icon="mdi-magnify"
-                  placeholder="Pesquisar aqui"></v-text-field>
-              </v-col>
-            </v-row>
-      </div>
+    <div class="header px-10 py-2">
+      <v-app-bar color="primary" class="black--text" app outlined flat dark>
+        <v-row>
+          <v-col cols="12" class="text-end py-5">
+            <v-btn
+              depressed
+              color="secondary"
+              class="px-6"
+              @click="$router.push('/login')"
+              ><v-icon size="20" class="mr-2">far fa-user-circle</v-icon
+              >Login</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-app-bar>
+      <v-row>
+        <v-col cols="4" class="py-0">
+          <div class="bg-logo" @click="$router.push('/')"></div>
+        </v-col>
+        <v-col cols="7" align="center">
+          <v-btn text color="secondary"
+            ><a class="nav-link" href="#dineduc">DinEduc</a></v-btn
+          >
+          <v-btn text color="secondary"
+            ><a class="nav-link" href="#classes">Aulas</a></v-btn
+          >
+          <v-btn text color="secondary"
+            ><a class="nav-link" href="#schoolPartnership"
+              >Parcerias Escolas</a
+            ></v-btn
+          >
+          <v-btn text color="secondary"
+            ><a class="nav-link" href="#volunteers">Voluntários</a></v-btn
+          >
+        </v-col>
+      </v-row>
+    </div>
   </v-main>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
 .main {
-  height: 13rem;
+  height: 11rem;
 }
 
-.header{
-    width: 100%;
-    height: 60px
+.header {
+  width: 100%;
+  height: 60px;
 }
 
-.bg-logo{
+.bg-logo {
   width: 100%;
   height: 70px;
-  background: url('../../assets/DinEducLogo.png') left no-repeat;
+  background: url("../../assets/DinEducLogo.png") left no-repeat;
   background-size: contain;
+}
+
+.nav-link {
+  text-decoration: none;
+  color: black !important;
 }
 </style>
