7d1d838da20273b450b8fe3ebda86636b1df85d7
Paloma Araujo <paclaraujo@gmail.com>
2022-01-17 22:42:29 -0300

    refactor: add app bar with login and chevron to back to home page

--- src/views/register/RegisterStudent.vue
+++ src/views/register/RegisterStudent.vue
@@ -1,15 +1,33 @@
 <template>
   <v-main>
-    <Header />
-    <v-container class="py-0 mb-12">
+    <v-app-bar color="primary" class="black--text" app outlined flat dark>
+      <v-row>
+        <v-col cols="4">
+          <v-btn icon color="black" text @click="$router.push('/')"
+            ><v-icon>mdi-chevron-left</v-icon></v-btn
+          >
+        </v-col>
+        <v-col cols="8" class="text-end py-5">
+          <v-btn
+            depressed
+            color="secondary"
+            class="px-6"
+            @click="$router.push('/login')"
+            ><v-icon size="20" class="mr-2">far fa-user-circle</v-icon
+            >Login</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-app-bar>
+    <v-container class="py-0 mt-6 mb-12">
       <v-card class="pa-10 text-center" fill-height tile>
         <v-row>
           <v-col cols="5">
             <div class="bg-register"></div>
           </v-col>
-          <v-col cols="7"> 
-            <div v-show="sigInShow">
-              <RegisterStudentForm :select="() => (sigInShow = false)" />
+          <v-col cols="7">
+            <div v-show="loginShow">
+              <RegisterStudentForm :select="() => (loginShow = false)" />
             </div>
           </v-col>
         </v-row>
@@ -20,25 +38,28 @@
 </template>
 
 <script>
-import Header from "@/components/communs/Header";
 import Footer from "@/components/communs/Footer";
 import RegisterStudentForm from "@/components/authentications/RegisterStudentForm";
 
 export default {
   components: {
-    Header,
     RegisterStudentForm,
     Footer,
   },
   data() {
     return {
-      sigInShow: true,
+      loginShow: true,
     };
   },
 };
 </script>
 
 <style scoped>
+.header {
+  width: 100%;
+  height: 10rem;
+}
+
 .size-100 {
   width: 100%;
   height: 100%;
